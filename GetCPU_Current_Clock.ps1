c3c7301c19fce6dd6650232e2d0cadfee10d217c
Capt Daags <bytesailor@protonmail.ch>
2020-10-05 11:57:42 +1100

    Update GetCPU_Current_Clock.ps1
    Added comments and attribution

--- GetCPU_Current_Clock.ps1
+++ GetCPU_Current_Clock.ps1
@@ -1,3 +1,8 @@
+
+# This gets the actual current CPU clock speed rather than just the base speed.
+# This is handy to see if the CPU is being throttled from the base speed.
+# Source: Senor-Rico
+
 $MaxClockSpeed = (Get-CimInstance CIM_Processor).MaxClockSpeed
 $ProcessorPerformance = (Get-Counter -Counter "\Processor Information(_Total)\% Processor Performance").CounterSamples.CookedValue
 $CurrentClockSpeed = $MaxClockSpeed*($ProcessorPerformance/100)
