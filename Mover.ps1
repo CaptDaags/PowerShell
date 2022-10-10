2240aea318786e0d281411e71ae731c664100adc
Daags <darrinpearce@gmail.com>
2022-10-10 13:45:33 +1100

    First commit

---
+++ Mover.ps1
@@ -0,0 +1,17 @@
+﻿
+# Mover.ps1
+# This mimics user activity
+# For $cycles send a numlock and another numlock key press
+# every -Seconds. Note the number of cycles is effectively
+# multiplied by -Seconds will tell how long it will run.
+# ie 600 cycles is about 5hrs
+
+
+param($cycles = 2400)
+
+$myshell = New-Object -com "Wscript.Shell"
+
+for ($i = 0; $i -lt $cycles; $i++) {
+  Start-Sleep -Seconds 15
+  $myshell.sendkeys("{NUMLOCK}{NUMLOCK}")
+}
